fd9a8336199a33a296352d5f825a65feca09e77f
JornNeiss <Jornneiss@gmail.com>
2024-11-30 20:22:52 +0100

    Add project files.

---
+++ Info_Systeem_Eventplanner/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Info_Systeem_Eventplanner.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
